446c338a72677b4025e68b58724b9e719497abdb
tomatowithpotato <1107682451@qq.com>
2022-07-10 21:30:42 +0800

    oh

--- haha_json/jsonTypeCast.h
+++ haha_json/jsonTypeCast.h
@@ -2,6 +2,10 @@
 
 using namespace haha::json;
 
+namespace haha{
+
+namespace json
+{
 
 /* 类型映射转换 */
 template<typename S> struct TypeMap;
@@ -22,46 +26,9 @@ struct isMatchType{
     }
 };
 
-// template<typename V>
-// struct isMatchType<JsonString, V>{
-//     bool operator() (){
-//         return std::is_same<V, std::string>::value;
-//     }
-// };
+} // namespace json
 
-// template<typename V>
-// struct isMatchType<JsonNumber, V>{
-//     bool operator() (){
-//         return std::is_same<V, int>::value || std::is_same<V, double>::value;
-//     }
-// };
 
-// template<typename V>
-// struct isMatchType<JsonBoolean, V>{
-//     bool operator() (){
-//         return std::is_same<V, bool>::value;
-//     }
-// };
-
-// template<typename V>
-// struct isMatchType<JsonNull, V>{
-//     bool operator() (){
-//         return std::is_same<V, decltype(nullptr)>::value;
-//     }
-// };
-
-// template<typename V>
-// struct isMatchType<JsonArray, V>{
-//     bool operator() (){
-//         return std::is_same<V, JsonArray::ValueType>::value;
-//     }
-// };
-
-// template<typename V>
-// struct isMatchType<JsonObject, V>{
-//     bool operator() (){
-//         return std::is_same<V, JsonObject::ValueType>::value;
-//     }
-// };
+}
 
 
